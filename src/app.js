f1e08f07ef24a7fba04e1e1d2d95f683cf1bdd46
bhuvaneshwaran <bhuvaneshwaran8996@gmail.com>
2019-12-10 07:25:00 +0530

    hjg

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
  
-app.get('/', function (req, res) {
+const Port = process.env.PORT || 3000
+app.get('/otha', function (req, res) {
   res.send('Hello World')
 })
  
-app.listen(3000,()=>{
+app.listen(Port,()=>{
     console.log("Port 3000 is running");
 })
