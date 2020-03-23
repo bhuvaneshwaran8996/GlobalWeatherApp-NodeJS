e369a13115a4c36c1477c386bd5b97eadd3c5f84
bhuvaneshwaran <bhuvaneshwaran8996@gmail.com>
2020-03-23 15:24:11 +0530

    zszsas

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 
 
-mongoose.connect(process.env.MONGODB_URL,{
+mongoose.connect('mongodb://127.0.0.1:27017/WeatherDevice',{
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
